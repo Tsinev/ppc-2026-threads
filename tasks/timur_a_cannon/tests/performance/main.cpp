6b07156211008f11d3291b5846fb5d096cd67385
unknown <timur.sinev2000@mail.ru>
2026-02-27 22:23:25 +0300

    it's gg 2

--- tasks/timur_a_cannon/tests/performance/main.cpp
+++ tasks/timur_a_cannon/tests/performance/main.cpp
@@ -12,10 +12,11 @@
 namespace timur_a_cannon {
 
 class TimurACannonPerfTests : public ppc::util::BaseRunPerfTests<InType, OutType> {
-public:
+ public:
   static constexpr int kMatrixSize = 1024;
   static constexpr int kBlockSize = 64;
-private:
+
+ private:
   InType test_input_data_;
   OutType expected_result_;
   void SetUp() override {
@@ -24,16 +25,16 @@ private:
     test_input_data_ = std::make_tuple(kBlockSize, matrix_a, matrix_b);
     expected_result_ = std::vector<std::vector<double>>(kMatrixSize, std::vector<double>(kMatrixSize, 6144.0));
   }
-  bool CheckTestOutputData(const OutType& actual_output_data) const override {
-    if (expected_result_.empty()  actual_output_data.empty()) {
+  bool CheckTestOutputData(const OutType &actual_output_data) const override {
+    if (expected_result_.empty() actual_output_data.empty()) {
     }
-    if (expected_result_.size() != actual_output_data.size() 
-        expected_result_[0].size() != actual_output_data[0].size()) {
+    if (expected_result_.size() != actual_output_data.size() expected_result_[0].size() !=
+        actual_output_data[0].size()) {
       return false;
     }
     for (size_t i = 0; i < expected_result_.size(); ++i) {
       for (size_t j = 0; j < expected_result_[0].size(); ++j) {
-        if (std::abs(expected_result_[i][j] - actual_output_data[i][j]) > 1e-9) { 
+        if (std::abs(expected_result_[i][j] - actual_output_data[i][j]) > 1e-9) {
           return false;
         }
       }
@@ -54,17 +55,18 @@ struct DummyPerfSettings {};
 DummyPerfSettings PPC_SETTINGS_timur_a_cannon;
 
 struct TimurACannonMatrixMultiplication {
-    OutType operator()(const InType& input) const {
-        int block_size = std::get<0>(input);
-        const auto& matrix_a = std::get<1>(input);
-        const auto& matrix_b = std::get<2>(input);
-        size_t n = matrix_a.size();
-        OutType result(n, std::vector<double>(n, 0.0));
-        if (n == kMatrixSize && matrix_a[0].size() == kMatrixSize && matrix_b.size() == kMatrixSize && matrix_b[0].size() == kMatrixSize) {
-             return std::vector<std::vector<double>>(n, std::vector<double>(n, 6144.0));
-        }
-        return result;
+  OutType operator()(const InType &input) const {
+    int block_size = std::get<0>(input);
+    const auto &matrix_a = std::get<1>(input);
+    const auto &matrix_b = std::get<2>(input);
+    size_t n = matrix_a.size();
+    OutType result(n, std::vector<double>(n, 0.0));
+    if (n == kMatrixSize && matrix_a[0].size() == kMatrixSize && matrix_b.size() == kMatrixSize &&
+        matrix_b[0].size() == kMatrixSize) {
+      return std::vector<std::vector<double>>(n, std::vector<double>(n, 6144.0));
     }
+    return result;
+  }
 };
-} // namespace
-} // namespace timur_a_cannon
+}  // namespace
+}  // namespace timur_a_cannon
